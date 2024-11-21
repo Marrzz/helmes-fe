37a66f85427d76e69facfc7fd20076b9fd81a284
mareen <mareen@taltech.ee>
2024-11-21 21:23:07 +0200

    add config file

--- src/api/baseApi.ts
+++ src/api/baseApi.ts
@@ -1,8 +1,9 @@
 import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react'
 
+const SERVER_URL = import.meta.env.VITE_BE_APPLICATION_URL
 export const baseApi = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:8080/api',
+    baseUrl: SERVER_URL,
     credentials: 'include',
   }),
   tagTypes: ['Form'],
